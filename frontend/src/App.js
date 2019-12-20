4306663c719e019756400825d2d9bfb4b03db067
Orianne Tanguy <otanguy28@gmail.com>
2019-12-20 01:05:11 +0100

    landing page structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,15 @@ import { Switch, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Main from "./components/Main";
 import Snow from "react-snow-effect";
-
+import "./App.scss"
 function App() {
-  return (
-    <div className="App">
+  return <div className="App">
       <Snow />
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/main" component={Main} />
       </Switch>
     </div>
-  );
 }
 
 export default App;
